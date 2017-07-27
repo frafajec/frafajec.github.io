60c5a601e2c6fff9b551f650b7bde552b3435dd3
Filip Rafajec <filip.rafajec@gmail.com>
2017-07-27 21:09:50 +0200

    Added eslint, scss and scssm

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scssm";
+
+function App() {
+  return (
+    <div styleName="App">
+      <div styleName="App-header">
+        <img src={logo} styleName="App-logo" alt="logo" />
+        <h2 styleName="bla">Welcome to React</h2>
+      </div>
+      <p styleName="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
